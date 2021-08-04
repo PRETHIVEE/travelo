4dc5853bbe5a6b8d53cf48e8c4eb0102657e908a
PRETHIVEE <kgprethivee@gmail.com>
2021-08-04 11:10:13 +0530

    created object in model and passed to html

--- tours/views.py
+++ tours/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Destination
 
 # Create your views here.
 def home(request):
-    # return HttpResponse('its working')
-    return render(request, 'index.html')
+    dest1 = Destination()
+    dest1.days = 3
+    dest1.location = 'Trivandrum'
+    dest1.country = 'Kerala'
+    dest1.price = 3333
+
+    return render(request, 'index.html', {'dest1':dest1})
