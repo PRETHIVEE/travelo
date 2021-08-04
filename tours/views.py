b61fd0a0b73f72748cef864fcd7ec3f7314a1e6b
PRETHIVEE <kgprethivee@gmail.com>
2021-08-04 15:48:45 +0530

    media path created

--- tours/views.py
+++ tours/views.py
@@ -4,34 +4,8 @@ from .models import Destination
 
 # Create your views here.
 def home(request):
-    dest1 = Destination()
-    dest1.img = 'pic4.jpg'
-    dest1.days = 3
-    dest1.location = 'Trivandrum'
-    dest1.country = 'Kerala'
-    dest1.price = 3333
+    
 
-    dest2 = Destination()
-    dest2.img = 'pic1.jpg'
-    dest2.days = 5
-    dest2.location = 'banglore'
-    dest2.country = 'karnataka'
-    dest2.price = 5500
-
-    dest3 = Destination()
-    dest3.img = 'pic3.jpg'
-    dest3.days = 4
-    dest3.location = 'Coorg'
-    dest3.country = 'karnataka'
-    dest3.price = 9999
-
-    dest4 = Destination()
-    dest4.img = 'pic2.jpg'
-    dest4.days = 6
-    dest4.location = 'banglore'
-    dest4.country = 'karnataka'
-    dest4.price = 5590
-
-    dests = [dest1, dest2, dest3, dest4]
+    dests = Destination.objects.all()
 
     return render(request, 'index.html', {'dests':dests})
